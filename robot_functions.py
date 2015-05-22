89b39dbeb38cf1c9af2367c763b57e24365d530b
OMARI1988 <omari.1988@gmail.com>
2015-05-22 18:23:25 +0100

    bla

--- robot_functions.py
+++ robot_functions.py
@@ -451,7 +451,7 @@ class Robot():
         # Create a Bitmap that will later on hold the screenshot image
 	    # Note that the Bitmap must have a size big enough to hold the screenshot
 	    # -1 means using the current default colour depth
-        bmp = wx.EmptyBitmap(self.display.width,self.display.height-20)
+        bmp = wx.EmptyBitmap(self.display.width,self.display.height-60)
 
 	    # Create a memory DC that will be used for actually taking the screenshot
         memDC = wx.MemoryDC()
@@ -465,10 +465,10 @@ class Robot():
         memDC.Blit( 0, # Copy to this X coordinate
         0, # Copy to this Y coordinate
         self.display.width, # Copy this width
-        self.display.height-20, # Copy this height
+        self.display.height-60, # Copy this height
         dcSource, # From where do we copy?
         self.display.x, # What's the X offset in the original DC?
-        self.display.y+20  # What's the Y offset in the original DC?
+        self.display.y+60  # What's the Y offset in the original DC?
         )
 
 	    # Select the Bitmap out of the memory DC by selecting a new
